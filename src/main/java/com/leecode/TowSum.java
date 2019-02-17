eda95ca51e5eef1c86b073c47d22121f00208ef6
winv <188508918@qq.com>
2019-02-17 15:29:36 +0800

    2数之和

--- src/main/java/com/leecode/TowSum.java
+++ src/main/java/com/leecode/TowSum.java
@@ -1,5 +1,8 @@
 package com.leecode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TowSum {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -8,6 +11,32 @@ public class TowSum {
                     return new int[] { i, j };
                 }
             }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public int[] twoSumHash(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public int[] twoSumHashOne(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
     }
--- src/main/java/com/winv/main.java
+++ src/main/java/com/winv/main.java
@@ -8,7 +8,11 @@ public class main {
 
         TowSum ts = new TowSum();
         int[] input = new int[]{2, 7, 9, 11};
-        int[] result = ts.twoSum(input, 9);
-        System.out.println(String.format("暴力解法:{0},{1}", result[0], result[1]));
+        int[] result = ts.twoSum(input, 11);
+        System.out.println(String.format("暴力解法:%d,%d", result[0], result[1]));
+        result = ts.twoSumHash(input, 11);
+        System.out.println(String.format("hash遍历:%d,%d", result[0], result[1]));
+        result = ts.twoSumHashOne(input, 11);
+        System.out.println(String.format("一轮Hash遍历:%d,%d", result[0], result[1]));
     }
 }
